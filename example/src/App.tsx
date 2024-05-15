9544c6eb670a387ba4fc15d16f02c4d4d36f88e6
Pedro Barreira <pbarreirat@outlook.com>
2024-05-14 21:10:18 -0300

    feat: text input

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 
-import { View } from 'react-native';
 import { Box, TextInput } from '../../src/components';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Box padding="xs" width="100%" align="center" direction="row">
+    <Box justify="center" align="center" flex={1}>
+      <Box padding="md" width="100%" align="center" direction="row">
         <TextInput label="Olá mundo" placeholder="Olá mundo" secureTextEntry />
       </Box>
-    </View>
+    </Box>
   );
 }
